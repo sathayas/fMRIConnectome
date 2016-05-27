0e3433999b8263ed28dc659131e590537630e4be
Satoru Hayasaka <hayasaka@utexas.edu>
2016-05-27 14:48:23 -0500

    The usage is re-formated so that it can be read by the help command

--- fsl_bet_wrapper.py
+++ fsl_bet_wrapper.py
@@ -7,14 +7,17 @@
 import os
 
 def run_bet(fBrain, fThresh=0.3):
-    #
-    # A wapper function for FSL's BET.
-    # Input Parameters:
-    #     fBrain:     The file name for the high-res T1 image
-    #     fThresh:    The fractional intensity threshold. Although FSL's
-    #                 default is 0.5, this function uses 0.3 by default.
-    #
-    # 
+    ''' A wapper function for FSL's BET.
+    
+    Input Parameters:
+          fBrain:     The file name for the high-res T1 image
+          fThresh:    The fractional intensity threshold. Although FSL's
+                      default is 0.5, this function uses 0.3 by default.
+
+    Returns:
+          NONE
+    '''
+
     # file name business
     WorkDir, fHighRes = os.path.split(fBrain)
     tmpfname, tmpext = os.path.splitext(fHighRes)
@@ -22,8 +25,10 @@ def run_bet(fBrain, fThresh=0.3):
         # the extension is .nii.gz
         tmpfname, tmpext = os.path.splitext(tmpfname)
     fOutBrain = os.path.join(WorkDir, tmpfname+'_brain') # output file name
+
     # assemblying the command
     com_bet = 'bet ' + fBrain + ' ' + fOutBrain
     com_bet += ' -R -f ' + str(fThresh) + ' -g 0'
+
     # calling bet
     res = os.system(com_bet)
