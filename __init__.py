9bd847bcd4277b4a24c06386f34db0d320877fc2
Satoru Hayasaka <hayasaka@utexas.edu>
2016-05-31 16:24:51 -0500

    Added some functions so that we can look up the help documentation

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 
 
 from .fsl_bet_wrapper import run_bet
-from .fsl_feat_wrapper import run_feat
-from .warp_fmri import run_warp
+from .fsl_feat_wrapper import run_feat, make_feat_design
+from .warp_fmri import run_warp, apply_warp, reslice_fmri
+
 
