68e6bcffa40a428a481ec2f7ae2f1368a4de334d
Satoru Hayasaka <hayasaka@utexas.edu>
2016-06-03 15:24:34 -0500

    Bandpass filtering functions were added

--- __init__.py
+++ __init__.py
@@ -3,5 +3,5 @@
 from .fsl_bet_wrapper import run_bet
 from .fsl_feat_wrapper import run_feat, make_feat_design
 from .warp_fmri import run_warp, apply_warp, reslice_fmri
-
+from .bp_filter import run_bp, bandpass_fMRI
 
